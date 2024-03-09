20087d015e9860a82de231dab706a7505ee849d2
Catzilla <jewaysongonzalgo@gmail.com>
2024-03-09 22:33:00 +0800

    Remove extra pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,32 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	let defaultDate = new Date();
+
+	// TODO: make manifest.json for top level
+
+
+	console.log(defaultDate.toLocaleDateString());
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Expense Tracker</title>
+	<meta name="description" content="Track your expense" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<div>
+	<input type="date" value={defaultDate} placeholder="select date"/>
+	<input type="number" placeholder="set price" />
+	<input type="text" placeholder="description" />
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+	<button>Record</button>
+</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
 
-	h1 {
+	input {
 		width: 100%;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
+	button {
 		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
