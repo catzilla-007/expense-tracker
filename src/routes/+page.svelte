a6c3da8f718a0be4836ad5262deaf686c2e05461
Catzilla <jewaysongonzalgo@gmail.com>
2024-04-02 16:33:00 +0800

    Convert indentation to spaces

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,70 @@
 <script lang="ts">
-	import { addExpense } from '$lib/api/sheets';
+  import { addExpense } from '$lib/api/sheets';
 
-	let date: string = formatDate(new Date());
-	let name: string = '';
-	let price: string = '';
-	let description: string = '';
+  let date: string = formatDate(new Date());
+  let name: string = '';
+  let price: string = '';
+  let description: string = '';
 
-	function formatDate(date: Date): string {
-		const parsedDate: Array<string> = date.toLocaleDateString().split('/');
-		return `${parsedDate[2]}-${parsedDate[0].padStart(2, '0')}-${parsedDate[1].padStart(2, '0')}`;
-	}
+  function formatDate(date: Date): string {
+    const parsedDate: Array<string> = date.toLocaleDateString().split('/');
+    return `${parsedDate[2]}-${parsedDate[0].padStart(2, '0')}-${parsedDate[1].padStart(2, '0')}`;
+  }
 
-	function sanitizeDateRequest(date: string) {
-		const parsedDate: Array<string> = date.split('-');
-		return `${parsedDate[1]}/${parsedDate[2]}/${parsedDate[0]}`;
-	}
+  function sanitizeDateRequest(date: string) {
+    const parsedDate: Array<string> = date.split('-');
+    return `${parsedDate[1]}/${parsedDate[2]}/${parsedDate[0]}`;
+  }
 
-	function handleRecordClick() {
-		const priceNum = parseFloat(price);
+  function handleRecordClick() {
+    const priceNum = parseFloat(price);
 
-		addExpense(name, priceNum, description, sanitizeDateRequest(date))
-			.then(() => {
-				console.log('expense added');
-			})
-			.catch(() => {
-				console.log('expense add failed');
-			})
-			.finally(() => {
-				name = '';
-				price = '';
-				description = '';
-			});
+    // remove addExpense request
 
-		// experimental
+    // addExpense(name, priceNum, description, sanitizeDateRequest(date))
+    // 	.then(() => {
+    // 		console.log('expense added');
+    // 	})
+    // 	.catch(() => {
+    // 		console.log('expense add failed');
+    // 	})
+    // 	.finally(() => {
+    // 		name = '';
+    // 		price = '';
+    // 		description = '';
+    // 	});
 
-		if (!navigator.serviceWorker) return;
+    if (!navigator.serviceWorker) return;
 
-		console.log('navigator is ok');
-		navigator.serviceWorker.ready.then((registration) => {
-			console.log('no registration active');
-			if (!registration.active) return;
-			console.log('registration active is ok');
-			registration.active.postMessage({ name: 'test' });
-		});
-	}
+    console.log('navigator is ok');
+    navigator.serviceWorker.ready
+      .then((registration) => {
+        console.log('no registration active');
+        if (!registration.active) return;
+        console.log('registration active is ok');
+        const expense = {
+          name,
+          price: priceNum,
+          description,
+          date: sanitizeDateRequest(date)
+        };
+        registration.active.postMessage(expense);
+      })
+      .catch((error) => {
+        console.error('some error happened', error);
+      })
+      .finally(() => {
+        name = '';
+        price = '';
+        description = '';
+        console.log('resetting values');
+      });
+  }
 </script>
 
 <svelte:head>
-	<title>Expense Tracker</title>
-	<meta name="description" content="Track your expense" />
+  <title>Expense Tracker</title>
+  <meta name="description" content="Track your expense" />
 </svelte:head>
 
 <input type="date" bind:value={date} placeholder="select date" />
@@ -59,25 +75,25 @@
 <button on:click={handleRecordClick}>Record</button>
 
 <style>
-	input {
-		height: 4rem;
-		font-size: 2rem;
-		text-align: center;
-		margin-bottom: 1rem;
-		border-radius: 1rem;
-	}
+  input {
+    height: 4rem;
+    font-size: 2rem;
+    text-align: center;
+    margin-bottom: 1rem;
+    border-radius: 1rem;
+  }
 
-	input[type='date'] {
-		text-align: center;
-	}
+  input[type='date'] {
+    text-align: center;
+  }
 
-	input[type='date']::-webkit-date-and-time-value {
-		text-align: center;
-	}
+  input[type='date']::-webkit-date-and-time-value {
+    text-align: center;
+  }
 
-	button {
-		height: 4rem;
-		font-size: 2rem;
-		border-radius: 1rem;
-	}
+  button {
+    height: 4rem;
+    font-size: 2rem;
+    border-radius: 1rem;
+  }
 </style>
--- src/service-worker.js
+++ src/service-worker.js
@@ -6,132 +6,132 @@ const CACHE = `cache-${version}`;
 const ASSETS = [...build, ...files];
 
 self.addEventListener('install', (event) => {
-	async function addFilesToCache() {
-		const cache = await caches.open(CACHE);
-		await cache.addAll(ASSETS);
-	}
-	event.waitUntil(addFilesToCache());
+  async function addFilesToCache() {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(ASSETS);
+  }
+  event.waitUntil(addFilesToCache());
 });
 
 self.addEventListener('activate', (event) => {
-	async function deleteOldCaches() {
-		for (const key of await caches.keys()) {
-			if (key !== CACHE) await caches.delete(key);
-		}
-	}
-	initializeDb();
-	event.waitUntil(deleteOldCaches());
+  async function deleteOldCaches() {
+    for (const key of await caches.keys()) {
+      if (key !== CACHE) await caches.delete(key);
+    }
+  }
+  initializeDb();
+  event.waitUntil(deleteOldCaches());
 });
 
 self.addEventListener('fetch', (event) => {
-	// only accept GET requests
-	if (event.request.method !== 'GET') return;
+  // only accept GET requests
+  if (event.request.method !== 'GET') return;
 
-	// ignore chrome dev tools
-	if (event.request.url.startsWith('chrome-extension')) return;
+  // ignore chrome dev tools
+  if (event.request.url.startsWith('chrome-extension')) return;
 
-	async function respond() {
-		const url = new URL(event.request.url);
-		const cache = await caches.open(CACHE);
+  async function respond() {
+    const url = new URL(event.request.url);
+    const cache = await caches.open(CACHE);
 
-		// `build`/`files` can always be served from the cache
-		if (ASSETS.includes(url.pathname)) {
-			const response = await cache.match(url.pathname);
+    // `build`/`files` can always be served from the cache
+    if (ASSETS.includes(url.pathname)) {
+      const response = await cache.match(url.pathname);
 
-			if (response) {
-				return response;
-			}
-		}
+      if (response) {
+        return response;
+      }
+    }
 
-		// for everything else, try the network first, but
-		// fall back to the cache if we're offline
-		try {
-			const response = await fetch(event.request);
+    // for everything else, try the network first, but
+    // fall back to the cache if we're offline
+    try {
+      const response = await fetch(event.request);
 
-			// if we're offline, fetch can return a value that is not a Response
-			// instead of throwing - and we can't pass this non-Response to respondWith
-			if (!(response instanceof Response)) {
-				throw new Error('invalid response from fetch');
-			}
+      // if we're offline, fetch can return a value that is not a Response
+      // instead of throwing - and we can't pass this non-Response to respondWith
+      if (!(response instanceof Response)) {
+        throw new Error('invalid response from fetch');
+      }
 
-			if (response.status === 200) {
-				cache.put(event.request, response.clone());
-			}
+      if (response.status === 200) {
+        cache.put(event.request, response.clone());
+      }
 
-			return response;
-		} catch (err) {
-			// save expense details to indexDB when fetch failed
-			if (url.host === 'script.google.com') {
-				const name = url.searchParams.get('name');
-				const price = url.searchParams.get('price');
-				const description = url.searchParams.get('description');
-				const date = url.searchParams.get('date');
+      return response;
+    } catch (err) {
+      // save expense details to indexDB when fetch failed
+      if (url.host === 'script.google.com') {
+        const name = url.searchParams.get('name');
+        const price = url.searchParams.get('price');
+        const description = url.searchParams.get('description');
+        const date = url.searchParams.get('date');
 
-				addExpense(name, price, description, date);
-				return new Response({ body: { status: 'saved to idb' } });
-			}
+        addExpense(name, price, description, date);
+        return new Response({ body: { status: 'saved to idb' } });
+      }
 
-			const response = await cache.match(event.request);
+      const response = await cache.match(event.request);
 
-			if (response) {
-				return response;
-			}
+      if (response) {
+        return response;
+      }
 
-			// if there's no cache, then just error out
-			// as there is nothing we can do to respond to this request
-			throw err;
-		}
-	}
+      // if there's no cache, then just error out
+      // as there is nothing we can do to respond to this request
+      throw err;
+    }
+  }
 
-	event.respondWith(respond());
+  event.respondWith(respond());
 });
 
 self.addEventListener('message', (event) => {
-	console.log(`The client sent me a message: ${event.data}`);
+  console.log(`The client sent me a message: ${event.data}`);
 });
 
 let db;
 
 export function initializeDb() {
-	const request = indexedDB.open('expenseDB', 1);
+  const request = indexedDB.open('expenseDB', 1);
 
-	request.onerror = (event) => {
-		console.log('cannot initializeDB');
-		console.log(event.target);
-	};
+  request.onerror = (event) => {
+    console.log('cannot initializeDB');
+    console.log(event.target);
+  };
 
-	request.onsuccess = (event) => {
-		console.log('initialize db successful');
-		db = event.target.result;
-	};
+  request.onsuccess = (event) => {
+    console.log('initialize db successful');
+    db = event.target.result;
+  };
 
-	request.onupgradeneeded = (event) => {
-		console.log('upgrading db');
-		db = event.target.result;
-		db.createObjectStore('expense', { autoIncrement: true });
-	};
+  request.onupgradeneeded = (event) => {
+    console.log('upgrading db');
+    db = event.target.result;
+    db.createObjectStore('expense', { autoIncrement: true });
+  };
 }
 
 export function addExpense(name, price, description, date) {
-	const transaction = db.transaction(['expense'], 'readwrite');
+  const transaction = db.transaction(['expense'], 'readwrite');
 
-	transaction.oncomplete = (event) => {
-		console.log('transaction complete', event);
-	};
+  transaction.oncomplete = (event) => {
+    console.log('transaction complete', event);
+  };
 
-	transaction.onerror = (event) => {
-		console.log('transaction failed', event);
-	};
+  transaction.onerror = (event) => {
+    console.log('transaction failed', event);
+  };
 
-	const objectStore = transaction.objectStore('expense');
+  const objectStore = transaction.objectStore('expense');
 
-	const request = objectStore.add({ name, price, description, date });
+  const request = objectStore.add({ name, price, description, date });
 
-	request.onsuccess = (event) => {
-		console.log('request sucess', event);
-	};
+  request.onsuccess = (event) => {
+    console.log('request sucess', event);
+  };
 
-	request.onerror = (event) => {
-		console.log('request failed', event);
-	};
+  request.onerror = (event) => {
+    console.log('request failed', event);
+  };
 }
